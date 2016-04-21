1b8315040d92a6f3619bedc52140689750b3524a
Troy Whorten <sabotuer99@gmail.com>
2016-04-21 20:12:12 +0000

    ordered answers desc

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,7 @@ class QuestionsController < ApplicationController
     
     def show
         @question = Question.find(params[:id])
+        @answers = @question.answers.order(created_at: :desc)
     end
     
     
