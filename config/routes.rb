c232fadeebca9482c6b8bfe0f05bcc55af8e21ce
Troy Whorten <sabotuer99@gmail.com>
2016-04-21 19:42:32 +0000

    updated create and show actions for questions

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   
   get 'about' => 'home#about'
-  
-  get '/questions/:id' => 'home#question'
-  post '/questions' => 'home#temp'
-  post '/answers' => 'home#temp'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
