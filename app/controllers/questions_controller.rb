c6994cee0f012e3af8037afebd8066790fe7a1dd
Troy Whorten <sabotuer99@gmail.com>
2016-04-21 20:57:15 +0000

    added message for no questions and answers, pulled rememberd email from session

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
     
     def create
         Question.create(question_params)
-        
+        session[:current_user_email] = question_params[:email]
         redirect_to root_path
     end
     
